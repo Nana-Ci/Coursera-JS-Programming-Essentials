a5570d9319f14d19bf2e2d23eab879bbb57e1298
Nana-Ci <amatsuda@kf7.so-net.ne.jp>
2025-11-09 17:02:58 -0500

    Extra controlFlow code added

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -55,3 +55,36 @@ switch (userType) {
 }
 
 console.log("User Category:", userCategory);
+
+/**
+ * Suppose an organization arranges a "Dietary Services" program to provide diets to its employees and customers, based on a person's weight and day-to-day routine. You need to create an authorization-based code to provide access to people based on their roles in organization, such as employees, enrolled members for "Dietary Services," and subscribers.
+
+If the person is an Employee, they are authorized to have access to "Dietary Services".
+
+If the person is an Enrolled Member, they are authorized to have access to "Dietary Services" and one-on-one interaction with a dietician.
+
+If the person is a Subscriber, they are authorized to have partial access to facilitate "Dietary Services" only.
+
+If the person is a Non-Subscriber, they need to enroll or at least subscribe first to avail this facility.
+ */
+let employeeStatus = "Employee";  // You can change this to test other roles
+let authorization;
+
+switch (employeeStatus) {
+  case "Employee":
+    authorization = "Full access to Dietary Services";
+    break;
+  case "Enrolled Member":
+    authorization = "Access to Dietary Services and one-on-one interaction with a dietician";
+    break;
+  case "Subscriber":
+    authorization = "Partial access to Dietary Services";
+    break;
+  case "Non-Subscriber":
+    authorization = "Please enroll or subscribe to access Dietary Services";
+    break;
+  default:
+    authorization = "Role not recognized. Access denied.";
+}
+
+console.log(authorization);
