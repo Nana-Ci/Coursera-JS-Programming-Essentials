0d937a1378ca58d318f47b526b0408160a912b8b
Nana-Ci <amatsuda@kf7.so-net.ne.jp>
2025-11-10 03:54:05 -0500

    Added extra excerciese code

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -9,5 +9,16 @@ function calculateArea() {
 let area = length * width;
 
 document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
-// 
+// The backticks and ${} notation allow for the inclusion of JavaScript variables within a string (using template literals).
+}
+
+
+function calculateTotal(){
+    grocery1 = parseFloat(document.getElementById('grocery1').value);
+    grocery2 = parseFloat(document.getElementById('grocery2').value);
+    grocery3 = parseFloat(document.getElementById('grocery3').value);
+
+    let total = grocery1 + grocery2 + grocery3;
+
+    document.getElementById('total').innerText = `The total amount is:$ ${total}`;
 }
